294320ee423d2e0bf013c7c58d3a3a5bd0d8ebd3
Olexandr Savchenko <a.savchenko1207@gmail.com>
2023-09-26 22:24:14 +0300

    03 task

---
+++ js/task-01.js
@@ -0,0 +1,10 @@
+const list = document.querySelector("#categories");
+const items = [...list.children];
+console.log(`Number of categories: ${items.length}`);
+
+items.forEach((item) => {
+  const title = item.querySelector("h2").textContent;
+  const categories = item.querySelectorAll("ul li").length;
+  console.log(`Category: ${title}`);
+  console.log(`Elements: ${categories}`);
+});
